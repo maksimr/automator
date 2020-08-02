90f3d9fc1aef404ad303719b0ec74a8b366196e2
Maksim Ryzhikov <rv.maksim@gmail.com>
2020-08-02 20:24:28 +0300

    chore: move rename to actions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 function main() {
-  const Workspace = require("../lib/Workspace").Workspace;
   const fs = require("memfs");
   fs.mkdirSync("/a");
   fs.mkdirSync("/a/d");
@@ -7,35 +6,20 @@ function main() {
   fs.writeFileSync("/a/c.js", "require('./d/e.js');");
   fs.writeFileSync("/a/d/e.js", "var x = 'foo'; x;");
 
-  const w = new Workspace("/a", { reader: fs });
-  w.ls("**/*.js").then(function(files) {
+  const Workspace = require("../lib/Workspace").Workspace;
+  const project = new Workspace("/a", { reader: fs });
+
+  project.ls("**/*.js").then(function(files) {
     console.log(files);
   });
 
-  w.transform("/a/d/e.js", node => {
-    return rename(node, "x", "y");
-  }).then(code => {
-    console.log(code);
-  });
-
-  /**
-   * @param {*} node
-   * @param {string} varName
-   * @param {string} newVarName
-   * @param {*=} scope
-   * @returns {*}
-   */
-  function rename(node, varName, newVarName, scope = null) {
-    return require("recast").visit(node, {
-      visitIdentifier(path) {
-        const name = path.node.name;
-        if (name === varName && (!scope || path.scope.lookup(name) === scope)) {
-          path.get("name").replace(newVarName);
-        }
-        return false;
-      }
+  project
+    .transform("/a/d/e.js", node => {
+      return require("../lib/actions/rename")(node, "x", "y");
+    })
+    .then(code => {
+      console.log(code);
     });
-  }
 }
 
 main();
