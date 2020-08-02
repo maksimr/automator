4e5c8e97e14aa28fa534dbcb61bd7b408f01770e
Maksim Ryzhikov <rv.maksim@gmail.com>
2020-08-02 18:12:24 +0300

    chore: add rename var example

--- src/index.js
+++ src/index.js
@@ -80,12 +80,30 @@ function main() {
   fs.mkdirSync("/a/d");
   fs.writeFileSync("/a/b.js", "require('./c');");
   fs.writeFileSync("/a/c.js", "require('./d/e.js');");
-  fs.writeFileSync("/a/d/e.js", "var x = 'foo'");
+  fs.writeFileSync("/a/d/e.js", "var x = 'foo'; x;");
 
   const w = new Workspace("/a", { reader: fs });
   w.ls("**/*.js").then(function(files) {
-    w.transform(files[0], node => node).then(console.log);
+    console.log(files);
   });
+
+  w.transform("/a/d/e.js", node => {
+    return rename(node, "x", "y");
+  }).then(code => {
+    console.log(code);
+  });
+
+  function rename(node, varName, newVarName, scope = null) {
+    return require("recast").visit(node, {
+      visitIdentifier(path) {
+        const name = path.node.name;
+        if (name === varName && (!scope || path.scope.lookup(name) === scope)) {
+          path.get("name").replace(newVarName);
+        }
+        return false;
+      }
+    });
+  }
 }
 
 main();
