50df861b8cbd654c8d7f19ca3897f79d3c7d4453
Maksim Ryzhikov <rv.maksim@gmail.com>
2020-08-03 15:00:34 +0300

    refactor: use single quote in project

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 function main() {
-  const fs = require("memfs");
-  fs.mkdirSync("/a");
-  fs.mkdirSync("/a/d");
-  fs.writeFileSync("/a/b.js", "require('./c');");
-  fs.writeFileSync("/a/c.js", "require('./d/e.js');");
-  fs.writeFileSync("/a/d/e.js", "var x = 'foo'; x;");
+  const fs = require('memfs');
+  fs.mkdirSync('/a');
+  fs.mkdirSync('/a/d');
+  fs.writeFileSync('/a/b.js', 'require("./c");');
+  fs.writeFileSync('/a/c.js', 'require("./d/e.js");');
+  fs.writeFileSync('/a/d/e.js', 'var x = "foo"; x;');
 
-  const Workspace = require("../lib/Workspace").Workspace;
-  const project = new Workspace("/a", { reader: fs });
+  const Workspace = require('../lib/Workspace').Workspace;
+  const project = new Workspace('/a', { reader: fs });
 
-  project.ls("**/*.js").then(function(files) {
+  project.ls('**/*.js').then(function(files) {
     console.log(files);
   });
 
   project
-    .transform("/a/d/e.js", node => {
-      return require("../lib/node/rename")(node, "x", "y");
+    .transform('/a/d/e.js', node => {
+      return require('../lib/node/rename')(node, 'x', 'y');
     })
     .then(code => {
       console.log(code);
